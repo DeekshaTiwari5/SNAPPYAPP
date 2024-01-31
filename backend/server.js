4bf5ccc336a07f7176e88a3ac165aa8ceb3d0fcd
Deeksha Tiwari <tdeeksha617@gmail.com>
2024-01-31 10:28:27 +0530

    build

--- backend/server.js
+++ backend/server.js
@@ -29,31 +29,16 @@ app.use('/api/message', messageRoutes);
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "frontend", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname1, "frontend", "build", "index.html"));
-  });
-} else {
-  const publicPath = path.join(__dirname1, "frontend", "public");
-  app.use(express.static(publicPath));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  // Serve your main HTML file for both root and other routes
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(publicPath, "index.html"));
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+});
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
   });
 }
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-//   app.get("*", (req, res) =>{
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-// });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
 
 
 // --------------------------deployment------------------------------
